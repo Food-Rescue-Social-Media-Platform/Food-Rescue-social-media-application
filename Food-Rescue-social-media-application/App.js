4f2539316fea43f4aebec9bb495df19e9bdf56bd
mohammadBelbesi <my_1999@live.com>
2024-02-28 22:47:41 +0200

    init our project

---
+++ Food-Rescue-social-media-application/App.js
@@ -0,0 +1,20 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>hello world me and my brother elyahu will develop an android, ios and web application </Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});

1246623a408d020ed0f39ee9064bb0748b3e2bf8
mohammadBelbesi <my_1999@live.com>
2024-03-03 20:15:02 +0200

    navigation dependencies

---
+++ socialApp/screens/OnboardingScreen.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import {View, Text, Button, StyleSheet} from 'react-native';
+
+const OnboardingScreen = () => {
+    return (
+        <View>
+            
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+
+})
+
+export default OnboardingScreen;

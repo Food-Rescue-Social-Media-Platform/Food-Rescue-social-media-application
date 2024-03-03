4f6c83e848c0330ea12951f85757fedb7281a8b5
mohammadBelbesi <my_1999@live.com>
2024-03-04 00:35:43 +0200

    feat: adding the navigate to the app

---
+++ socialApp/screens/LoginScreen.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import {View, Text, Button, StyleSheet} from 'react-native';
+
+const LoginScreen = () => {
+    return (
+        <View style={styles.container}>
+            <Text>Login Screen</Text>
+            <Button
+                title="Click Here"
+                onPress={() => alert('Button Clicked! ')}
+            />
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+});
+
+export default LoginScreen;

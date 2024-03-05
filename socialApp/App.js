ea1d27432df82bea700a3e70fdab87270b173dc0
mohammadBelbesi <my_1999@live.com>
2024-03-05 18:12:15 +0200

    feat: Onboarding Screen

--- socialApp/App.js
+++ socialApp/App.js
@@ -1,8 +1,9 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect } from 'react';
+import { Platform, StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 import LoginScreen from './screens/LoginScreen';
 import OnboardingScreen from './screens/OnboardingScreen';
@@ -10,15 +11,44 @@ import OnboardingScreen from './screens/OnboardingScreen';
 const AppStack = createStackNavigator();
 
 export default function App() {
-  return (
-    <NavigationContainer>
-      <AppStack.Navigator headerShown="none">
-        <AppStack.Screen name="Onboarding" component={OnboardingScreen}/>
-        <AppStack.Screen name="Login" component={LoginScreen}/>
-      </AppStack.Navigator>
-    </NavigationContainer>
-  );
+  const [isFirstLaunch, setIsFirstLaunch] = React.useState(null);
+
+  useEffect(() => {
+    AsyncStorage.getItem('alreadyLaunched').then(value => {
+      if(value == null){
+        AsyncStorage.setItem('alreadyLaunched','true');
+        setIsFirstLaunch(true);
+      }
+      else{
+        setIsFirstLaunch(false);
+      }
+    })
+  },[]);
+
+  if(isFirstLaunch=== null){
+    return null;
+  }
+  else if( isFirstLaunch === true){
+    return (
+      <NavigationContainer>
+        <AppStack.Navigator 
+          screenOptions={{
+            headerShown: false
+          }}
+          >
+          {Platform.OS === 'android' || Platform.OS === 'ios' ? (
+            <AppStack.Screen name="Onboarding" component={OnboardingScreen}/>
+          ) : null}
+          <AppStack.Screen name="Login" component={LoginScreen}/>
+        </AppStack.Navigator>
+      </NavigationContainer>
+    );
+  }
+  else{
+    return <LoginScreen/>
+  }
 }
+
 // Move the StyleSheet.create outside of the component
 const styles = StyleSheet.create({
   container: {
--- socialApp/screens/OnboardingScreen.jsx
+++ socialApp/screens/OnboardingScreen.jsx
@@ -1,15 +1,42 @@
 import React from 'react';
-import {View, Text, Button, StyleSheet} from 'react-native';
+import {View, Text, Button, Image, TouchableOpacity, StyleSheet} from 'react-native';
+
+import Onboarding from 'react-native-onboarding-swiper';
+
+const Done = ({... props}) => (
+    <TouchableOpacity 
+        style={{marginHorizontal:8}}
+        {... props}
+    ><Text style={{fontSize:16, marginHorizontal:8, color:'#000000'}}>Done</Text></TouchableOpacity>
+);
 
 const OnboardingScreen = ({navigation}) => {
     return (
-        <View style={styles.container}>
-            <Text>Onboarding Screen</Text>
-            <Button
-                title="Click Here"
-                onPress={() => navigation.navigate("Login")}
-            />
-        </View>
+        <Onboarding
+        DoneButtonComponent={Done}
+        onSkip={() => navigation.replace("Login")}
+        onDone={() => navigation.navigate("Login")}
+        pages={[
+            {
+            backgroundColor: '#e9bcbe',
+            image: <Image source={require('../assets/onboarding-img1.png')} />,
+            title: 'Connect to our Awesome Community',
+            subtitle: 'A New Way to Save Food from Wasting',
+            },
+            {
+            backgroundColor: '#fdeb93',
+            image: <Image source={require('../assets/onboarding-img2.png')} />,
+            title: 'Share Your Food',
+            subtitle: 'Share your Surplus Food',
+            },
+            {
+            backgroundColor: '#a6e4d0',
+            image: <Image source={require('../assets/onboarding-img3.png')} />,
+            title: 'Become An Advertising Star',
+            subtitle: 'Be One of the Food Rescuer to Reducing Food Waste',
+            },
+        ]}
+        />
     );
 };
 

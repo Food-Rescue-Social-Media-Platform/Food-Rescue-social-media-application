ef9010e03561be11e6a878ee36f68bec15e5f768
mohammadBelbesi <my_1999@live.com>
2024-03-06 03:05:10 +0200

    update: make the app file empty and use routes and navigation instead

--- socialApp/App.js
+++ socialApp/App.js
@@ -1,60 +1,8 @@
-import { StatusBar } from 'expo-status-bar';
-import React, { useEffect } from 'react';
-import { Platform, StyleSheet, Text, View } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import AsyncStorage from '@react-native-async-storage/async-storage';
+import React from "react";
+import Providers from './navigation'
 
-import LoginScreen from './screens/LoginScreen';
-import OnboardingScreen from './screens/OnboardingScreen';
-
-const AppStack = createStackNavigator();
-
-export default function App() {
-  const [isFirstLaunch, setIsFirstLaunch] = React.useState(null);
-
-  useEffect(() => {
-    AsyncStorage.getItem('alreadyLaunched').then(value => {
-      if(value == null){
-        AsyncStorage.setItem('alreadyLaunched','true');
-        setIsFirstLaunch(true);
-      }
-      else{
-        setIsFirstLaunch(false);
-      }
-    })
-  },[]);
-
-  if(isFirstLaunch=== null){
-    return null;
-  }
-  else if( isFirstLaunch === true){
-    return (
-      <NavigationContainer>
-        <AppStack.Navigator 
-          screenOptions={{
-            headerShown: false
-          }}
-          >
-          {Platform.OS === 'android' || Platform.OS === 'ios' ? (
-            <AppStack.Screen name="Onboarding" component={OnboardingScreen}/>
-          ) : null}
-          <AppStack.Screen name="Login" component={LoginScreen}/>
-        </AppStack.Navigator>
-      </NavigationContainer>
-    );
-  }
-  else{
-    return <LoginScreen/>
-  }
+const App = () => {
+  return <Providers />
 }
 
-// Move the StyleSheet.create outside of the component
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
---
+++ socialApp/navigation/AuthStack.js
@@ -0,0 +1,68 @@
+import React, { useEffect } from 'react';
+import { Platform, StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+import LoginScreen from '../screens/LoginScreen';
+import SignUpScreen from '../screens/SignUpScreen';
+import OnboardingScreen from '../screens/OnboardingScreen';
+
+const Stack = createStackNavigator();
+
+const AuthStack = () => {
+  const [isFirstLaunch, setIsFirstLaunch] = React.useState(null);
+  let routeName;
+
+  useEffect(() => {
+    AsyncStorage.getItem('alreadyLaunched').then((value) => {
+      if(value == null){
+        AsyncStorage.setItem('alreadyLaunched','true');
+        setIsFirstLaunch(true);
+      }
+      else{
+        setIsFirstLaunch(false);
+      }
+    })
+  },[]);
+
+  if(isFirstLaunch=== null){
+    return null;
+  }
+  else if( isFirstLaunch === true){
+    routeName = 'Onboarding';
+  }
+  else{
+    routeName = 'Login';
+  }
+
+  return(
+    <Stack.Navigator initialRouteName={routeName}>
+        {Platform.OS === 'android' || Platform.OS === 'ios' ? (
+        <Stack.Screen
+            name = "Onboarding"
+            component={OnboardingScreen}
+            options={{header: () => null}}
+        />
+        ) : null}        
+        <Stack.Screen
+            name = "Login"
+            component={LoginScreen}
+            options={{header: () => null}}
+        />
+        <Stack.Screen name="SignUp" component={SignUpScreen}/>
+    </Stack.Navigator>
+  );
+};
+
+export default AuthStack;
+
+// Move the StyleSheet.create outside of the component
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});

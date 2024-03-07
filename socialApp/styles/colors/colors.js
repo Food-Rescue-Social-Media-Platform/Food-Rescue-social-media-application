42f5dacfa78134add2a5630bb13048f299b05f7f
Eliyahu-Yishai <eli089743@gmail.com>
2024-03-07 08:34:45 +0200

    add colors global

---
+++ socialApp/styles/colors/colors.js
@@ -0,0 +1,6 @@
+export const primaryColor = '#CEF0D3';
+export const secondaryColor = '#A7EAAE';
+export const navBar = '#56D06B';
+export const headerChat = '#A7EAAE';
+export const messageNotME = '#E5E5E5';
+export const searchChat  = '#6BB178';

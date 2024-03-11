bf23b664b12f3f88272dfb70b37345cac79a6278
Eliyahu-Yishai <eli089743@gmail.com>
2024-03-11 21:57:06 +0200

    add option back to chatHeader

--- socialApp/components/header/ChatHeader.jsx
+++ socialApp/components/header/ChatHeader.jsx
@@ -3,26 +3,23 @@ import { useState } from "react";
 import {COLORS} from "../../styles/colors";
 import { windowHeight } from "../../utils/Dimentions";
 import FontAwesome6 from 'react-native-vector-icons/FontAwesome6';
+import Fontisto from 'react-native-vector-icons/Fontisto';
 
-const ChatHeader = (props) => {
-    const { data } = props;
-
-    const [userName , setUserName] = useState(data);
+const ChatHeader = ({isSingleChatPage, user ,showOptionsIcon = true ,onOptionsClick, onReturnClick}) => {
+    const [userName , setUserName] = useState(user?.userName || 'eliyahu');
     const [userImage , setUserImage] = useState('https://www.elitesingles.co.uk/wp-content/uploads/sites/59/2019/11/2b_en_articleslide_sm2-350x264.jpg');
-
-    const handleClickOptions = () => {
-       console.log('Options Clicked');
-    }
 
     return(
         <View style={styled.container}>
+            <Fontisto name="angle-left" size={20} onPress={()=>{onReturnClick}}/>   
             <StatusBar barStyle="light-content" backgroundColor={COLORS.theme} translucent={false} />
-            { data !== 'Chat' ? (<Image style={styled.profileImg} src={userImage}/>): null}
-            <Text style={[data === 'Chat'? (styled.chatHome):(styled.userName)]}>{userName}</Text>
-            { data !== 'Chat' ? (<FontAwesome6 name="ellipsis-vertical" size={20} onPress={()=>{handleClickOptions}}/>): null}
+            { isSingleChatPage ? (<Image style={styled.profileImg} src={userImage}/>): null}
+            <Text style={[isSingleChatPage? (styled.userName):(styled.chatHome)]}>{userName}</Text>
+            { showOptionsIcon? (<FontAwesome6 name="ellipsis-vertical" size={20} onPress={()=>{onOptionsClick}}/>): null}
          </View>
     );
 };
+
 
 const styled = StyleSheet.create({
     container:{
@@ -39,12 +36,13 @@ const styled = StyleSheet.create({
     },
     userName:{
         fontSize: 20,
-        marginLeft: 10,
-        width: "77%"
+        marginLeft: 7,
+        width: "70%"
     },
     chatHome:{
-      fontSize: 20,
-      marginLeft: 20,
+        fontSize: 20,
+        marginLeft: 20,
+        width: "75%"
     }
 });
 

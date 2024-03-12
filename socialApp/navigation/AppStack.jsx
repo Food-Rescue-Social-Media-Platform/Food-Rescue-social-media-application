0642455ff61ebc18081b98ff180ab6da88b81473
Eliyahu-Yishai <eli089743@gmail.com>
2024-03-12 13:22:39 +0200

    add navigation from home to chats

--- socialApp/navigation/AppStack.jsx
+++ socialApp/navigation/AppStack.jsx
@@ -1,14 +1,15 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { View, StyleSheet } from 'react-native';
 import { createStackNavigator } from '@react-navigation/stack';
 import HomeScreen from '../screens/HomeScreen';
+import Chats from '../screens/chat/Chats';
 
 const Stack = createStackNavigator();
-
 const AppStack = () => {
     return (
         <Stack.Navigator>
-            <Stack.Screen name='Home' component={HomeScreen} />
+        <Stack.Screen name='Home' component={HomeScreen} />
+        <Stack.Screen name='Chats' component={Chats}/>
         </Stack.Navigator>
     );
 }

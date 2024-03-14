2062986a73a328b1cd79907d9ba910e2060b6153
Eliyahu-Yishai <eli089743@gmail.com>
2024-03-14 09:27:59 +0200

    add provider to app

--- socialApp/navigation/index.jsx
+++ socialApp/navigation/index.jsx
@@ -1,13 +1,23 @@
 import React from "react";
 import Routes from './Routes';
 import { AuthProvider } from "./AuthProvider";
+import { Provider } from 'react-redux';
+import {AppRegistry} from 'react-native';
+import {name as appName} from '../app.json';
+import store from '../redux/store'; 
+import App from '../App';
 
-const Providers = () => {
+const Providers = () => {    
     return (
+        <Provider store={store}>
         <AuthProvider>
-            <Routes />
+        <Routes />
         </AuthProvider>
-    );
-}
-
+        </Provider>
+        );
+    }
+    
+AppRegistry.registerComponent(appName, () => Providers);
 export default Providers;
+
+

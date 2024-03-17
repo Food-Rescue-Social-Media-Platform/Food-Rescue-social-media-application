e114bc4fd612e0dd73e1e8467d6cf92b32808347
mohammadBelbesi <my_1999@live.com>
2024-03-17 17:26:53 +0200

    add: Safe Area Provider for bottom stack navigator

--- socialApp/App.js
+++ socialApp/App.js
@@ -1,8 +1,15 @@
 import React from "react";
 import Providers from './navigation'
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 const App = () => {
-  return <Providers />
+  
+  return (
+    <SafeAreaProvider>
+    <Providers />
+    </SafeAreaProvider>
+
+  )
 }
 
 export default App;

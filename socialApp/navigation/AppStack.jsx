64bb6efe7ba501e583ab40744a0727ba9ecf2d70
Eliyahu-Yishai <eli089743@gmail.com>
2024-03-17 10:35:36 +0200

    add stack screen to other components

--- socialApp/navigation/AppStack.jsx
+++ socialApp/navigation/AppStack.jsx
@@ -1,15 +1,27 @@
 import React, {useState} from 'react';
-import { View, StyleSheet } from 'react-native';
+import { View, StyleSheet, Text } from 'react-native';
 import { createStackNavigator } from '@react-navigation/stack';
 import HomeScreen from '../screens/HomeScreen';
-import Chats from '../screens/chat/Chats';
+import Chat from '../screens/chat/Chat';
+import HomeChat from '../screens/chat/HomeChat';
+import SingleChat from '../screens/chat/SingleChat';
+import { Button } from 'react-native-elements';
+ 
 
 const Stack = createStackNavigator();
 const AppStack = () => {
     return (
         <Stack.Navigator>
-        <Stack.Screen name='Home' component={HomeScreen} />
-        <Stack.Screen name='Chats' component={Chats}/>
+            <Stack.Screen name='Home' component={HomeScreen} />
+            <Stack.Screen name='Chat' component={Chat} />
+            <Stack.Screen name='HomeChat' component={HomeChat} />
+            <Stack.Screen name='SingleChat' component={SingleChat}
+                options={ ({route}) => ({
+                    headerTitle: () => {
+                        const receiverName = route.params?.receiverData?.firstName === ''? (route.params?.chatData?.name): (route.params?.receiverData?.firstName);
+                        <Text>{receiverName}</Text>},
+                })}
+            />
         </Stack.Navigator>
     );
 }

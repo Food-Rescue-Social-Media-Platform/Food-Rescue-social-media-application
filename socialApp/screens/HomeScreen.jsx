084c9d501c8dbecbf41cedeca0c200b35ae39449
mohammadBelbesi <my_1999@live.com>
2024-03-19 00:09:24 +0200

    feat: post card : 1st style

--- socialApp/screens/HomeScreen.jsx
+++ socialApp/screens/HomeScreen.jsx
@@ -1,32 +1,141 @@
 import React,{useContext} from 'react';
-import {View, StyleSheet,Text} from 'react-native';
+import {View, StyleSheet,Text,TouchableOpacity} from 'react-native';
 import FormButton from '../components/FormButton';
 import { AuthContext } from '../navigation/AuthProvider';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import SimpleLineIcons from 'react-native-vector-icons/SimpleLineIcons';
+import { Container,Card, UserInfo, UserImg, UserName, PostTime, UserInfoText, PostText, PostImg, InteractionWrapper, Divider} from '../styles/feedStyles';
 
 const HomeScreen = () => {
     const {user,logout} = useContext(AuthContext);
     return (
-        <View style={styles.container}>
-            <Text style={styles.text}>Welcome {user.uid}</Text>
+        <Container>
+            <Card>
+                <View style={{
+                        
+                        flexDirection: "row",
+                        justifyContent: "space-between",
+                    }}>
+                            <UserInfo>
+                        <UserImg source={require('../assets/users/user-1.jpg')}/>
+                        <UserInfoText>
+                            <UserName>mohammad salah</UserName>
+                            <PostTime>4 hours ago</PostTime>
+                        </UserInfoText>
+                        </UserInfo>
+                        <TouchableOpacity style={{
+                            paddingTop:25,
+                            paddingRight:20}}>
+                            <SimpleLineIcons
+                            name="options"
+                            size={24}
+                            />
+                        </TouchableOpacity>
+                        
+                                         
+                </View>
+
+                <PostImg source={require('../assets/posts/post-img-7.jpg')}/>
+                <InteractionWrapper>
+                    
+                    <View style={styles.iconsWrapper}>
+                        <MaterialCommunityIcons
+                        name="cupcake"
+                        size={22}
+                        />  
+                        <Text style={styles.text}>milky</Text>                  
+                    </View>
+                    <View style={styles.iconsWrapper}>
+                        <MaterialCommunityIcons
+                        name="clock"
+                        size={22}
+                        />  
+                        <Text style={styles.text}>Today - 13:50 AM</Text>                  
+                    </View>
+                    <View style={styles.iconsWrapper}>
+                        <MaterialCommunityIcons
+                        name="map-marker"
+                        size={22}
+                        />  
+                        <Text style={styles.text}>0.8 KM </Text>                  
+                    </View>
+                </InteractionWrapper>
+                <PostText>a pancakes 3 boxes. after a big party. we have these pancakes left,
+                          delicious pancakes that we made by pancakes lovers hands.
+                </PostText>
+            </Card>
+            <Card>
+                <View style={{
+                        
+                        flexDirection: "row",
+                        justifyContent: "space-between",
+                    }}>
+                            <UserInfo>
+                        <UserImg source={require('../assets/users/user-1.jpg')}/>
+                        <UserInfoText>
+                            <UserName>mohammad salah</UserName>
+                            <PostTime>4 hours ago</PostTime>
+                        </UserInfoText>
+                        </UserInfo>
+                        <TouchableOpacity style={{
+                            paddingTop:25,
+                            paddingRight:20}}>
+                            <SimpleLineIcons
+                            name="options"
+                            size={24}
+                            />
+                        </TouchableOpacity>
+                        
+                                         
+                </View>
+                <Divider></Divider>
+                {/* <PostImg source={require('../assets/posts/post-img-7.jpg')}/> */}
+                <InteractionWrapper>
+                    
+                    <View style={styles.iconsWrapper}>
+                        <MaterialCommunityIcons
+                        name="cupcake"
+                        size={22}
+                        />  
+                        <Text style={styles.text}>milky</Text>                  
+                    </View>
+                    <View style={styles.iconsWrapper}>
+                        <MaterialCommunityIcons
+                        name="clock"
+                        size={22}
+                        />  
+                        <Text style={styles.text}>Today - 13:50 AM</Text>                  
+                    </View>
+                    <View style={styles.iconsWrapper}>
+                        <MaterialCommunityIcons
+                        name="map-marker"
+                        size={22}
+                        />  
+                        <Text style={styles.text}>0.8 KM</Text>                  
+                    </View>
+                </InteractionWrapper>
+                <PostText>a pancakes 3 boxes. after a big party. we have these pancakes left,
+                          delicious pancakes that we made by pancakes lovers hands.
+                </PostText>
+            </Card>
             <FormButton buttonTitle='Logout' onPress={() => logout()} />
-        </View>
+        </Container>
+        
     );
 }
 
 export default HomeScreen;
 
 const styles = StyleSheet.create({
-    container: {
-        backgroundColor: '#f9fafd',
-        flex: 1,
-        justifyContent: 'center',
-        alignContent: 'center',
-        alignItems: 'center',
-        padding: 20,
-    },
     text: {
-        fontSize: 20,
-        color: '#333333'
+        fontSize: 12,
+        color: "black"
+    },
+
+    iconsWrapper: {
+        flexDirection: "row",
+        justifyContent: "space-between",
+        alignItems: "center"
     }
 });
 

1181213698900683ac31be6f3e3be2f2713c138f
mohammadBelbesi <my_1999@live.com>
2024-03-27 18:33:42 +0200

    fix: update all the css after changing the buttom navbar

--- socialApp/navigation/AppStack.jsx
+++ socialApp/navigation/AppStack.jsx
@@ -1,13 +1,11 @@
 import React from 'react';
-import { View, StyleSheet, Text, TouchableOpacity } from 'react-native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 import Ionicons from 'react-native-vector-icons/Ionicons';
-import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
 
 import HomeScreen from '../screens/HomeScreen';
-import ChatScreen from '../screens/ChatScreen ';
+import ChatScreen from '../screens/ChatScreen';
 import ProfileScreen from '../screens/ProfileScreen';
 import MessagesScreen from '../screens/MessagesScreen';
 import MapScreen from '../screens/MapScreen';
@@ -15,7 +13,7 @@ import MapScreen from '../screens/MapScreen';
 const Stack = createStackNavigator();
 const Tab = createBottomTabNavigator();
 
-const FeedStack = ({navigation}) => (
+const FeedStack = () => (
   <Stack.Navigator>
     <Stack.Screen
       name="HomePage"
@@ -24,7 +22,6 @@ const FeedStack = ({navigation}) => (
         headerTitleAlign: 'center',
         headerTitleStyle: {
           color: '#2e64e5', // Blue color
-          // fontSize: 18,
         },
         headerStyle: {
           backgroundColor: '#f9fafd', // Set background color here
@@ -46,11 +43,11 @@ const FeedStack = ({navigation}) => (
         },
         headerBackTitleVisible: false,
         headerBackImage: () => (
-          <View style={{marginLeft: 15}}>
+          <View style={{ marginLeft: 15 }}>
             <Ionicons name="arrow-back" size={25} color="#2e64e5" />
           </View>
         ),
-        headerTitleStyle: { // Blue color for the title
+        headerTitleStyle: {
           color: '#2e64e5',
         },
       }}
@@ -58,57 +55,18 @@ const FeedStack = ({navigation}) => (
   </Stack.Navigator>
 );
 
-const MessageStack = ({navigation}) => (
-  <Stack.Navigator>
-    <Stack.Screen 
-      name="Messages" 
-      component={MessagesScreen} 
-      options={{
-        headerTitleAlign: 'center',
-        headerTitleStyle: { // Blue color for the title
-          color: '#2e64e5',
-        },
-        headerStyle: {
-          backgroundColor: '#f9fafd', // Set background color here
-          shadowColor: '#fff',
-          elevation: 0,
-        },
-      }} 
-    />
-    <Stack.Screen
-      name="Chat"
-      component={ChatScreen}
-      options={({route}) => ({
-        headerTitleAlign: 'center',
-        title: route.params.userName,
-        headerBackTitleVisible: false,
-        headerTitleStyle: { // Blue color for the title
-          color: '#2e64e5',
-        },
-        headerStyle: {
-          backgroundColor: '#f9fafd', // Set background color here
-          shadowColor: '#fff',
-          elevation: 0,
-        },
-      }
-      )}
-    />
-  </Stack.Navigator>
-);
-
-const ProfileStack = ({navigation}) => (
+const ProfileStack = () => (
   <Stack.Navigator>
     <Stack.Screen
       name="Profile"
       component={ProfileScreen}
       options={{
         headerTitleAlign: 'center',
-        // headerShown: false,
-        headerTitleStyle: { // Blue color for the title
+        headerTitleStyle: {
           color: '#2e64e5',
         },
         headerStyle: {
-          backgroundColor: '#f9fafd', // Set background color here
+          backgroundColor: '#f9fafd',
           shadowColor: '#fff',
           elevation: 0,
         },
@@ -117,106 +75,121 @@ const ProfileStack = ({navigation}) => (
   </Stack.Navigator>
 );
 
-const MapStack = ({navigation}) => (
+const MapStack = () => (
   <Stack.Navigator>
     <Stack.Screen
       name="Map"
       component={MapScreen}
       options={{
-
         headerTitleAlign: 'center',
         headerTitleStyle: {
           color: '#2e64e5',
         },
         headerStyle: {
-          backgroundColor: '#f9fafd', // Set background color here
+          backgroundColor: '#f9fafd',
           shadowColor: '#fff',
           elevation: 0,
         },
-        
       }}
     />
   </Stack.Navigator>
 );
 
-
-
 const AppStack = () => {
+  return (
+    <Tab.Navigator initialRouteName="Home"
+    screenOptions={{
+      elevation: 0,
+      
+      tabBarStyle: {
+        backgroundColor: '#f9fafd',
+        elevation: 0,
+        height:55,
+        borderTopWidth: 0,
 
-return (
-    <Tab.Navigator
-    initialRouteName="Home">
-    
-    <Tab.Screen
-        screenOptions={{headerShown:false}}
+      },
+      tabBarItemStyle:{
+      marginBottom:10,
+    },
+      activeTintColor: 'green',
+      inactiveTintColor: '#000',
+    }}>
+      <Tab.Screen
         name="HomeTab"
         component={FeedStack}
-        options={({route}) => ({
-        tabBarLabel: 'Home',
-        headerShown: false ,
-        tabBarIcon: ({color, size}) => (
+        options={({ route }) => ({
+          tabBarLabel: 'Home',
+          headerShown: false ,
+          tabBarActiveTintColor: 'green',
+          tabBarInactiveTintColor: '#000',
+          tabBarIcon: ({ color, size }) => (
             <MaterialCommunityIcons
-            name="home-outline"
-            color={color}
-            size={25}
+              name="home-outline"
+              color={color}
+              size={25}
             />
-        ),
+          ),
         })}
-    />
-
-    <Tab.Screen
-      name="MapTab"
-      component={MapStack}
-      options={{
-        tabBarLabel: 'Map',
-        headerShown: false ,
-        tabBarIcon: ({color, size}) => (
-          <MaterialCommunityIcons
-            name="map-marker-outline"
-            color={color}
-            size={25}
-          />
-        ),
-        
-      }}
-    />
-
-    <Tab.Screen
+      />
+      <Tab.Screen
+        name="MapTab"
+        component={MapStack}
+        options={({ route }) => ({
+          tabBarLabel: 'Map',
+          headerShown: false ,
+          tabBarActiveTintColor: 'green',
+          tabBarInactiveTintColor: '#000',
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons
+              name="map-marker-outline"
+              color={color}
+              size={25}
+            />
+          ),
+        })}
+      />
+      <Tab.Screen
         name="MessagesTab"
-        component={MessageStack}
-        options={({route}) => ({
-        tabBarLabel: 'Messages',
-        headerShown: false ,
-
-        tabBarIcon: ({color, size}) => (
+        component={MessagesScreen}
+        options={({ route }) => ({
+          tabBarLabel: 'Messages',
+          headerShown: false ,
+          tabBarActiveTintColor: 'green',
+          tabBarInactiveTintColor: '#000',
+          tabBarIcon: ({ color, size }) => (
             <Ionicons
-            name="chatbox-ellipses-outline"
-            color={color}
-            size={25}
+              name="chatbox-ellipses-outline"
+              color={color}
+              size={25}
             />
-        ),
+          ),
         })}
-    />
-    <Tab.Screen
+      />
+
+      <Tab.Screen
         name="ProfileTab"
         component={ProfileStack}
-        options={({route}) => ({
-        tabBarLabel: 'Profile',
-        headerShown: false ,
-        tabBarStyle: {display: 'none'},
-        
-        tabBarIcon: ({color, size}) => (
-            <Ionicons name="person-outline" 
-            color={color}
-            size={25}
-            />
-        ),
+        options={({ route }) => ({
+          tabBarLabel: 'Profile',
+          headerShown: false ,
+          tabBarActiveTintColor: 'green',
+          tabBarInactiveTintColor: '#000',
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="person-outline" color={color} size={25} />
+          ),
         })}
-    />
+      />
     </Tab.Navigator>
-);
+  );
 };
-  
-const styles = StyleSheet.create({})
 
-export default AppStack;
+const RootStack = createStackNavigator();
+
+const RootStackScreen = () => (
+  <RootStack.Navigator>
+    <RootStack.Screen name="Main" component={AppStack} options={{ headerShown: false }}/>
+    <RootStack.Screen name="Chat" component={ChatScreen} />
+  </RootStack.Navigator>
+);
+
+export default RootStackScreen;

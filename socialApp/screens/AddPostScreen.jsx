37b2e86ba4b83576f22cc978852f0177fdd5754d
Eliyahu-Yishai <eli089743@gmail.com>
2024-03-28 18:43:07 +0200

    add function to open galere to add images

--- socialApp/screens/AddPostScreen.jsx
+++ socialApp/screens/AddPostScreen.jsx
@@ -1,37 +1,104 @@
 import React, {useState} from 'react';
-import { View, Modal,StyleSheet, Text, TextInput, TouchableOpacity } from 'react-native';
+import { View,StyleSheet, Text, TextInput, TouchableOpacity } from 'react-native';
 import { AntDesign } from '@expo/vector-icons';
-import { Button } from 'react-native-elements';
-import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 import Entypo from 'react-native-vector-icons/Entypo';
 import Fontisto from 'react-native-vector-icons/Fontisto';
 import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
 import FontAwesome6 from 'react-native-vector-icons/FontAwesome5';
 import {COLORS} from '../styles/colors';
-import {windowHeight, windowWidth} from '../utils/Dimentions';
-import { Image } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
-
+import firestore from '@react-native-firebase/firestore';
+import {Camera} from 'expo-camera'
+import * as ImagePicker from 'expo-image-picker';
+import { uploadImages } from '../helpers/UplaodImges';
 
 const AddPostScreen = () => {
     const navigation = useNavigation();
-    const [showModal, setShowModal] = useState(false);
+    const [image, setImage] = useState(null);
+    const [uploading, setUploading] = useState(false);
+    const [transferred, setTransferred] = useState(0);
+    const [post, setPost] = useState(null);
 
-    const openShareFoodScreen = () => {
-        navigation.navigate('AddPost');
-        console.log('Share food screen');
-        setShowModal(true);
-        console.log(showModal);
+    const handleClose = () => {
+        console.log('Close');
+    }
+
+    const handleOpenCamera = async () => {
+        console.log('Camera');
+    }
+    
+    const handleAddImages = async () => {
+      console.log('Images');
+      const s = await ImagePicker.getCameraPermissionsAsync();
+      // No permissions request is necessary for launching the image library
+       let result = await ImagePicker.launchImageLibraryAsync({
+        mediaTypes: ImagePicker.MediaTypeOptions.All,
+        allowsEditing: true,
+        // aspect: [4, 3],
+        quality: 1,
+      });
+    
+      if (!result.canceled) {
+        setImage(result.assets[0].uri);
+        console.log("uri: ", result.assets[0].uri);
+        await uploadImages(result.assets[0].uri, 'postsImges/');
+      }
+    }
+
+    const handleAddLocation = () => {
+        console.log('Location');
+    }
+
+    const handelAddClock = () => {
+        console.log('Clock');
+    }   
+
+    const handelAddPhone = () => {
+        console.log('Phone');
+    }
+
+    const handelAddCategory = () => {
+        console.log('Category');
     }
 
     const handleAddPost = () => {
-        console.log('Post');
+        // 1. check if have more without text
+        // 2. if have image save in storage
+        // 3. send request to fireStore to save post
+        // 4. navigate to home page
+        navigation.navigate('HomePage');
     }
 
-    const handleClose = () => {
-        setShowModal(false);
-    }
+    // const uploadImage = async (uri) => {
+    //     if(uri == null) {
+    //       return null;
+    //     }
 
+        
+    //     const response = await fetch(uri);
+    //     const blob = await response?.blob();
+
+    //     // let filename = uploadUri.substring(uploadUri.lastIndexOf('/') + 1);
+    
+    //     // // Add timestamp to File Name
+    //     // const extension = filename.split('.').pop(); 
+    //     // const name = filename.split('.').slice(0, -1).join('.');
+    //     // filename = name + Date.now() + '.' + extension; 
+
+    //     const storageRef = ref(storage, `postsImges/${ 'post_img_' + Date.now()}`)
+    //     uploadBytesResumable(storageRef, blob)
+    //     .then((snapshot) => {
+    //         // console.log('Uploaded a file!', snapshot.ref);
+    //         getDownloadURL(snapshot.ref).then((downloadURL) => {
+    //             console.log('File available at', downloadURL);
+    //         });
+    //     }).catch((error) => {
+    //         console.error('Error uploading image:', error);
+    //       });
+    // };
+
+
+    
     return (      
             <View style={styles.centeredView}>
                     <View style={styles.modalView}>
@@ -46,14 +113,27 @@ const AddPostScreen = () => {
                              placeholder="What's on your mind?"
                              multiline
                          />
-                         <Icons 
-                                size={20}
-                                color={'black'} 
-                                handelClick={openShareFoodScreen}
-                                iconStyle={styles.icon}
-                                wrapperStyle={styles.iconsWrapper}
-                                />
-                         <TouchableOpacity style={styles.button}>
+                         <View style={styles.iconsWrapper}>
+                            <TouchableOpacity>
+                            <Entypo name="camera" size={22} color='black' onPress={handleOpenCamera} style={styles.icon}/>
+                            </TouchableOpacity>
+                            <TouchableOpacity>
+                            <FontAwesome6 name="images" size={22} color='black' onPress={handleAddImages} style={styles.icon}/>
+                            </TouchableOpacity>
+                            <TouchableOpacity>
+                                <Entypo name="location-pin"  size={22} color='black' onPress={handleAddLocation} style={styles.icon}/>
+                            </TouchableOpacity>
+                            <TouchableOpacity >
+                                <Fontisto name="clock"  size={22} color='black' onPress={handelAddClock} style={styles.icon}/>
+                            </TouchableOpacity>
+                            <TouchableOpacity >
+                                <Entypo name="phone"  size={22} color='black' onPress={handelAddPhone} style={styles.icon}/>
+                            </TouchableOpacity>
+                            <TouchableOpacity >
+                                <MaterialIcons name="category"  size={22} color='black' onPress={handelAddCategory} style={styles.icon}/>
+                            </TouchableOpacity>
+                            </View>
+                         <TouchableOpacity style={styles.button} onPress={handleAddPost}>
                            <Text style={styles.buttonText}>Post</Text>
                          </TouchableOpacity>
                     </View>
@@ -73,17 +153,20 @@ const AddPostScreen = () => {
     },
     centeredView: {
         flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
+        width: '100%',
+        height: '100%',
+        // justifyContent: 'center',
+        // alignItems: 'center',
         backgroundColor: 'rgba(0,0,0,0.5)',
     },
     modalView: {
-        width: windowWidth/1.1,
-        height: windowHeight / 1.30,
+        flex: 1,
+        width: '100%',
+        height: '100%',
+        // justifyContent: 'center',
+        // alignItems: 'center',
         backgroundColor: 'white',
         padding: 20,
-        borderRadius: 10,
-        elevation: 5,
     },
     closeButton: {
         alignSelf: 'center',

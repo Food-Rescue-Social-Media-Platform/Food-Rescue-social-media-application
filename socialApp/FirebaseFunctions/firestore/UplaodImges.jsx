ff7064fd85282e8f8b214f641eeefb72fd3db39d
Eliyahu-Yishai <eli089743@gmail.com>
2024-04-01 09:12:24 +0300

    change: add parameter to function to get the url of image

--- socialApp/FirebaseFunctions/firestore/UplaodImges.jsx
+++ socialApp/FirebaseFunctions/firestore/UplaodImges.jsx
@@ -2,7 +2,7 @@ import {ref, getDownloadURL, uploadBytesResumable} from 'firebase/storage';
 import {storage} from '../../firebase.js';
 
 // Upload images to firebase storage
-export const uploadImages = async (uri, path, type) => {
+export const uploadImages = async (uri, path, type, setImageUrl) => {
     if(uri == null) {
       return null;
     }
@@ -13,12 +13,14 @@ export const uploadImages = async (uri, path, type) => {
     const pathForRef = path + Date.now(); 
 
     const storageRef = ref(storage, pathForRef);
-
+    
     uploadBytesResumable(storageRef, blob)
-    .then((snapshot) => {
-        getDownloadURL(snapshot.ref).then((downloadURL) => {
-            console.log('Uploaded a file! available at', downloadURL);
-        });
+    .then(async (snapshot) => {
+      setImageUrl(await getDownloadURL(snapshot.ref));
+        // await getDownloadURL(snapshot.ref).then((downloadURL) => {
+        //     console.log('Uploaded a file! available at', downloadURL);
+        //     setImageUrl(downloadURL);
+        // });
     }).catch((error) => {
         console.error('Error uploading image:', error);
       });

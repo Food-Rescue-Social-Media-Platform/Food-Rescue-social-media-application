56d79e2f2d54197399f04c4c0be420673e45efe4
Eliyahu-Yishai <eli089743@gmail.com>
2024-04-18 23:34:26 +0300

    fix: add option to function 'openGalereAndSelectImages' to select option to select images from galere

--- socialApp/FirebaseFunctions/OpeningComponentsInPhone.jsx
+++ socialApp/FirebaseFunctions/OpeningComponentsInPhone.jsx
@@ -2,21 +2,21 @@ import * as ImagePicker from 'expo-image-picker';
 
 // the function below is used to select images from the gallery
 // and set the image to the state setImage[]
-export const openGalereAndSelectImages = async (setImages) => {
-    const s = await ImagePicker.getCameraPermissionsAsync();
-    // No permissions request is necessary for launching the image library
-     let result = await ImagePicker.launchImageLibraryAsync({
-      mediaTypes: ImagePicker.MediaTypeOptions.All,
-      // allowsEditing: true,
-      allowsMultipleSelection: true,
-      // aspect: [4, 3],
-      quality: 1,
-    });
-  
-    if (!result.canceled) {
-      console.log("result: ", result);
-      setImages( oldImages => [...oldImages, result.assets[0].uri] );
-    }
+export const openGalereAndSelectImages = async (setImages, allowsMultipleSelection=true, allowsEditing=false, allMediaTypeOptionsOpen=true) => {
+  const s = await ImagePicker.getCameraPermissionsAsync();
+      // No permissions request is necessary for launching the image library
+      let result = await ImagePicker.launchImageLibraryAsync({
+        mediaTypes: allMediaTypeOptionsOpen ? ImagePicker.MediaTypeOptions.All : ImagePicker.MediaTypeOptions.Images,
+        allowsEditing: allowsEditing,
+        allowsMultipleSelection: allowsMultipleSelection,
+        // aspect: [4, 3],
+        quality: 1,
+      });
+    
+      if (!result.canceled) {
+        console.log("result: ", result);
+        setImages( oldImages => [...oldImages, result.assets[0].uri] );
+      }
   }
 
 // the function below is used to take a picture from the camera

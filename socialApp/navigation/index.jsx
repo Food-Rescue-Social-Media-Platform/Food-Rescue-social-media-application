75db0599213fda9decbdfe2214016b9ce6ff1ed9
Eliyahu-Yishai <eli089743@gmail.com>
2024-05-18 23:18:13 +0300

    fix some bugs in redux persist

--- socialApp/navigation/index.jsx
+++ socialApp/navigation/index.jsx
@@ -4,18 +4,15 @@ import { AuthProvider } from "./AuthProvider";
 import { Provider } from 'react-redux';
 import {AppRegistry} from 'react-native';
 import {name} from '../app.json';
-import { storePersist, persistor } from "../redux/store/configurationStore"; 
-import { PersistGate } from "redux-persist/integration/react";
+
 
 
 const Providers = () => {    
     return (
         <Provider store={storePersist}>
-             <PersistGate loading={null} persistor={persistor}>
                  <AuthProvider>
                        <Routes />
                  </AuthProvider>
-            </PersistGate>
         </Provider>
         );
     }
--- socialApp/redux/store/configurationStore.js
+++ socialApp/redux/store/configurationStore.js
@@ -1,24 +1,24 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
-import { persistStore, persistReducer } from 'redux-persist'
 import { configureStore } from '@reduxjs/toolkit';
 import reducer from '../reducer';
-import storage from 'redux-persist/lib/storage' 
-
+import storage from 'redux-persist/lib/storage';
+import { persistStore, persistReducer, FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER } from 'redux-persist';
 
 const persistConfig = {
   key: 'root',
   storage: AsyncStorage,
-}
+};
 
-const persistedReducer = persistReducer(persistConfig, reducer)
+const persistedReducer = persistReducer(persistConfig, reducer);
 
 export const storePersist = configureStore({
   reducer: persistedReducer,
-  middleware: (getDefaultMiddleware) => {
-    return getDefaultMiddleware({
-      serializableCheck: false
-    })
-  }
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
 
-export const persistor = persistStore(storePersist)
+export const persistor = persistStore(storePersist);

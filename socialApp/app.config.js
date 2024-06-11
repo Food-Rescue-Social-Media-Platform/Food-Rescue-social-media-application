83eed5f6d90974c815296bfa2cbd7479039988c6
mohammadBelbesi <my_1999@live.com>
2024-06-11 11:31:12 +0300

    map config

---
+++ socialApp/app.config.js
@@ -0,0 +1,12 @@
+export default {
+    expo: {
+      android: {
+        config: {
+          googleMaps: {
+            apiKey: "AIzaSyDsrEf0oqU7R84Ta6WvGf29klHMQbVBCJY"
+          }
+        }
+      }
+    }
+  };
+  

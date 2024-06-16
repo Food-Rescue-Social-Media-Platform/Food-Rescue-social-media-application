28ef93295a2edf1281a98b89957a8f77ce4f7cc3
Eliyahu-Yishai <eli089743@gmail.com>
2024-06-16 13:30:31 +0300

    feat: add buttons to select category and select radius  drawerContent

--- socialApp/navigation/CustomDrawerContent.jsx
+++ socialApp/navigation/CustomDrawerContent.jsx
@@ -10,6 +10,8 @@ import FormButton from '../components/formButtonsAndInput/FormButton';
 import { AuthContext } from './AuthProvider';
 import { categoriesList } from '../utils/categories';
 import { CheckBox } from 'react-native-elements';
+import Slider from '@react-native-community/slider';
+
 
 const CustomDrawerContent = (props) => {
   const { isDarkMode, setIsDarkMode, theme } = useDarkMode();
@@ -19,6 +21,10 @@ const CustomDrawerContent = (props) => {
   // const [ categories, setCategories ] = useState([]);
   const [ selectedCategories, setSelectedCategories ] = useState([]);
   const [categories, setCategories] = useState(categoriesList.map((category) => ({ value: category })));
+  const [ isForYou, setIsForYou ] = useState(true);
+  const [ categoriesFilterOpen, setCategoriesFilterOpen ] = useState(false);
+  const [ RadiusFilterOpen, setRadiusFilterOpen ] = useState(false);
+  const [ radius, setRadius ] = useState(10);
   const { logout } = useContext(AuthContext);
   const themeColors = isDarkMode ? DARKCOLORS : COLORS;
 
@@ -64,14 +70,30 @@ const CustomDrawerContent = (props) => {
   const changeFeedChoice = async (feed) => {
     try {
       setCurrentFeedChoice(feed);
+      if(feed === 'For You'){
+        setIsForYou(true);
+      } else {
+        setIsForYou(false);
+      }
     } catch (error) {
       console.error("Error changing feed choice:", error);
     }
   }
 
+  const handleClickCategory = (category) => {
+    if (selectedCategories.includes(category.value)) {
+      setSelectedCategories(selectedCategories.filter((item) => item !== category.value));
+    } else {
+      setSelectedCategories([...selectedCategories, category.value]);
+    }
+  }
+
+
   return (
     <DrawerContentScrollView {...props}>
-      <DrawerItemList {...props} />
+    
+    <DrawerItemList {...props} />
+    <View style={styles.container}>
       <View style={styles.feedChoiceContainer}>
         {feedChoice.map((feed) => (
             <TouchableOpacity
@@ -87,81 +109,134 @@ const CustomDrawerContent = (props) => {
         )}
       </View>
 
-      <View style={{ flex: 1, flexDirection: 'row', flexWrap: 'wrap' }}>
-          {categories.map((category) => (
-              <CheckBox
-                  style={[styles.checkboxWrapper, { backgroundColor: themeColors.secondaryBackground }]}
-                  key={category.value}
-                  title={category.value}
-                  checked={selectedCategories.includes(category.value)}
-                  onPress={() => handleCheck(category)}
-                  textStyle={{ color: themeColors.primaryText }}
-                  containerStyle={{ backgroundColor: themeColors.secondaryBackground }}
-              />
-          ))}
-      </View>
+      { isForYou ? (
+        <>
+          <View style={{flex:1, flexDirection:'cul'}}>
+              <TouchableOpacity
+                style={[styles.drawerItem]}
+                onPress={() => setCategoriesFilterOpen(!categoriesFilterOpen)}
+              >
+                   <Text style={{color: themeColors.primaryText, marginLeft: 10, marginTop: 10}}>Category Filter</Text>
+              </TouchableOpacity> 
+              
+              { categoriesFilterOpen &&
+                <>
+                <Text style={{color: themeColors.primaryText, margin: 17}}>Select Categories:</Text>
+                <View style={{ flex: 1, flexDirection: 'row', flexWrap: 'wrap', padding:4 , marginBottom:10}}>
+                        {categories.map((category) => (
+                            <CheckBox
+                                style={styles.checkboxWrapper}
+                                key={category.value}
+                                title={category.value}
+                                checked={selectedCategories.includes(category.value)}
+                                onPress={() => handleClickCategory(category)}
+                                containerStyle={[styles.checkboxWrapper, {backgroundColor: themeColors.white}]}
+                            />
+                    ))}
+                </View>
+                </>
+              }
 
-      <View style={styles.container}>
+              <TouchableOpacity
+                style={[styles.drawerItem]}
+                onPress={() => setRadiusFilterOpen(!RadiusFilterOpen)}
+              >
+                <Text style={{color: themeColors.primaryText, marginLeft: 10, marginTop: 10}}>Radius Filter</Text>
+              </TouchableOpacity> 
+
+             { RadiusFilterOpen && (
+                <View style={styles.sliderContainer}>
+                  <Text style={styles.sliderLabel}>Radius: {radius} km</Text>
+                  <Slider
+                    style={{ width: '100%', height: 40 }}
+                    minimumValue={10}
+                    maximumValue={50}
+                    step={1}
+                    value={radius}
+                    onValueChange={(value) => setRadius(value)}
+                    minimumTrackTintColor="#1EB1FC"
+                    maximumTrackTintColor="#d3d3d3"
+                    thumbTintColor="#1EB1FC"
+                  />
+                </View>
+            )}
+          </View> 
+      </>
+      ) : (null)}
+    
+
+      <View style={{marginTop:'30%'}}>
         <View style={styles.switchContainer}>
-          <Text style={{ marginLeft:-10,color: isDarkMode ? theme.lightGray : theme.primaryText }}>{t('Dark Mode')}</Text>
+          <Text style={{ color: isDarkMode ? theme.lightGray : theme.primaryText }}>{t('Dark Mode')}</Text>
           <Switch
             value={isDarkMode}
             onValueChange={(value) => setIsDarkMode(value)}
           />
         </View>
+
         <Text style={styles.SelectLanguage}>{t('Select Language')}:</Text>
         <View style={styles.languageButtonsContainer}>
-          {languages.map((language) => (
-            <TouchableOpacity
-              key={language.value}
-              style={[
-                styles.languageButton,
-                currentLanguage === language.value && styles.selectedLanguageButton,
-              ]}
-              onPress={() => changeLanguage(language.value)}
-            >
-              <Text style={{ color: currentLanguage === language.value ? '#FFF' : '#000' }}>
-                {language.label}
-              </Text>
-            </TouchableOpacity>
-          ))}
+            {languages.map((language) => (
+              <TouchableOpacity
+                key={language.value}
+                style={[
+                  styles.languageButton,
+                  currentLanguage === language.value && styles.selectedLanguageButton,
+                ]}
+                onPress={() => changeLanguage(language.value)}
+              >
+                <Text style={{ color: currentLanguage === language.value ? '#FFF' : '#000' }}>
+                  {language.label}
+                </Text>
+              </TouchableOpacity>
+            ))}
+        </View>
+        <View style={styles.logoutButton}>
+              <FormButton buttonTitle={t('logout')} onPress={() => logout()} />
+        </View>
         </View>
       </View>
-      <View style={styles.logoutButton}>
-            <FormButton buttonTitle={t('logout')} onPress={() => logout()} />
-      </View>
-
     </DrawerContentScrollView>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    justifyContent: 'flex-start',
-    padding: 20,
+    padding: 10,
   },
   feedChoiceContainer: {
     flexDirection: 'row',
     marginTop: 20,
+    marginBottom: 20,
   },
   drawerItem: {
-    padding: 10,
+    padding: 8,
+    margin: 5,
     backgroundColor: '#FFF',
     borderRadius: 8,
     borderWidth: 1,
     borderColor: '#444',
     flex: 1,
     alignItems: 'center',
-    marginHorizontal: 5,
+
   },
   selectedDrawerItem: {
     backgroundColor: '#007BFF', // Blue color for the selected button
   },
+  checkboxWrapper: {
+    padding: 4,
+  },
+  sliderContainer: {
+    padding: 20,
+  },
+  sliderLabel: {
+    textAlign: 'center',
+    marginBottom: 10,
+  },
   switchContainer: {
     flexDirection: 'row',
     alignItems: 'center',
-    marginBottom: 20,
+    marginBottom: 14,
   },
   languageButtonsContainer: {
     flexDirection: 'row',
@@ -183,10 +258,11 @@ const styles = StyleSheet.create({
   },
   SelectLanguage:{
     fontSize: 14,
-    marginLeft:-10,
   },
   logoutButton:{
-    marginLeft:10,
+    marginTop: 20,
+    width: '80%',
+    alignSelf: 'center',
   },
 });
 

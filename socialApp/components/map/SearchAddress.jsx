42a9044330219dc57b587f247102097bd1b435f8
Eliyahu-Yishai <eli089743@gmail.com>
2024-07-26 10:29:12 +0300

    fix merge

--- socialApp/components/map/SearchAddress.jsx
+++ socialApp/components/map/SearchAddress.jsx
@@ -56,6 +56,7 @@ function SearchAddress({style, onLocationSelected }) {
 const styles = StyleSheet.create({
   searchBox: {
     flex:1,
+    position: 'absolute',
     marginTop: 20,
     zIndex: 1,
     flexDirection: "row",

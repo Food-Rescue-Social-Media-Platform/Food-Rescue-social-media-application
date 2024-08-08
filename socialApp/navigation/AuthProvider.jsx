5dd7ed4c8cc360da91125ece6862d391f13105c0
mohammadBelbesi <my_1999@live.com>
2024-08-08 19:56:56 +0300

    feat: google auth first run

--- socialApp/navigation/AuthProvider.jsx
+++ socialApp/navigation/AuthProvider.jsx
@@ -1,24 +1,72 @@
 import React, { createContext, useState } from 'react';
 import { signInWithEmailAndPassword, createUserWithEmailAndPassword, sendPasswordResetEmail, signOut } from 'firebase/auth';
-import { auth, database } from '../firebase';
+import { auth, database, GoogleAuthProvider, signInWithCredential } from '../firebase';
 import { getDoc, setDoc, doc, serverTimestamp } from 'firebase/firestore';
 import { useDispatch } from 'react-redux';
 import { setUserData, removerUserData } from '../redux/reducer/user';
 import { FeedFollowers, addFeedFollowers } from '../FirebaseFunctions/collections/feedFollowers';
 import Toast from 'react-native-toast-message';
+import * as Google from 'expo-auth-session/providers/google';
+import * as WebBrowser from 'expo-web-browser';
+
+WebBrowser.maybeCompleteAuthSession();
 
 export const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
     const dispatch = useDispatch();
-
     const [user, setUser] = useState(null);
+
+    const [request, response, promptAsync] = Google.useAuthRequest({
+        expoClientId: '684711937854-ct1n3i8ak7dlb9co4vir7s58oo0jbf6k.apps.googleusercontent.com',
+        androidClientId: '684711937854-n0vv51uhqghlhvbepo9raa9g1t0k5qab.apps.googleusercontent.com',
+        webClientId: '684711937854-83lmp0kaol8jkjif24gokmcms9npia7t.apps.googleusercontent.com',
+    });
+
+    React.useEffect(() => {
+        if (response?.type === 'success') {
+            const { id_token, access_token } = response.params;
+
+            const credential = GoogleAuthProvider.credential(id_token, access_token);
+            signInWithCredential(auth, credential).then(async (firebaseUser) => {
+                const docRef = doc(database, 'users', firebaseUser.user.uid);
+                const docSnap = await getDoc(docRef);
+
+                if (docSnap.exists()) {
+                    let userData = docSnap.data();
+                    userData.id = firebaseUser.user.uid;
+                    dispatch(setUserData(userData));
+                } else {
+                    console.log("No such document!");
+                    throw new Error('No such document');
+                }
+
+                Toast.show({
+                    type: 'success',
+                    text1: 'Success',
+                    text2: 'Logged in with Google successfully.',
+                });
+            }).catch(error => {
+                console.log('error', error);
+                Toast.show({
+                    type: 'error',
+                    text1: 'Login Error',
+                    text2: 'An error occurred during Google sign-in.',
+                });
+            });
+        }
+    }, [response]);
+
+    const signInWithGoogle = async () => {
+        promptAsync();
+    };
 
     return (
         <AuthContext.Provider 
             value={{
                 user,
                 setUser,
+                signInWithGoogle,
 
                 login: async (email, password) => {
                     try {
@@ -37,7 +85,7 @@ export const AuthProvider = ({ children }) => {
                             throw new Error('No such document');
                         }
                     } catch (error) {
-                        throw error; // Rethrow the error so it can be caught in the component
+                        throw error;
                     }
                 },
 

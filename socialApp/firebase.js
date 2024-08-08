fc3e51575c01cbb19cc1ed23a6fe905e685d1ce4
Eliyahu-Yishai <eli089743@gmail.com>
2024-08-08 17:48:09 +0300

    chore: Update firebase.js with environment variables

--- socialApp/firebase.js
+++ socialApp/firebase.js
@@ -6,7 +6,7 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 import firebase from 'firebase/compat/app';
 import { getDatabase } from "firebase/database";
-import { API_KEY, AUTH_DOMAIN, PROJECT_ID, STORAGE_BUCKET, MESSAGING_SENDER_ID, APP_ID, MEASUREMENT_ID } from "@env";
+import { API_KEY, AUTH_DOMAIN, PROJECT_ID, STORAGE_BUCKET, MESSAGING_SENDER_ID, APP_ID, MEASUREMENT_ID } from '@env';
 
 const firebaseConfig = {
     apiKey: API_KEY,
@@ -16,6 +16,15 @@ const firebaseConfig = {
     messagingSenderId: MESSAGING_SENDER_ID,
     appId: APP_ID,
     measurementId: MEASUREMENT_ID
+    
+
+    // apiKey: "AIzaSyDiwU4JI2KtkDhg6uYMeL7125BmHtPQ_2A",
+    // authDomain: "food-rescue-social-platform.firebaseapp.com",
+    // projectId: "food-rescue-social-platform",
+    // storageBucket: "food-rescue-social-platform.appspot.com",
+    // messagingSenderId: "684711937854",
+    // appId: "1:684711937854:web:aa16c5dfb5ff0fe8f68e6f",
+    // measurementId: "G-P1SGVSNEJL"
 };
 
 export const app = initializeApp(firebaseConfig);

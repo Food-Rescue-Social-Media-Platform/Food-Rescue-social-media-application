ab9c7997be00f65cc17006c0fc62a88dcf8c76f7
Eliyahu-Yishai <eli089743@gmail.com>
2024-08-08 17:14:34 +0300

    chore: Update Google Maps API key in local.properties and .env file

--- socialApp/firebase.js
+++ socialApp/firebase.js
@@ -6,15 +6,16 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 import firebase from 'firebase/compat/app';
 import { getDatabase } from "firebase/database";
+import { API_KEY, AUTH_DOMAIN, PROJECT_ID, STORAGE_BUCKET, MESSAGING_SENDER_ID, APP_ID, MEASUREMENT_ID } from "@env";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDiwU4JI2KtkDhg6uYMeL7125BmHtPQ_2A",
-    authDomain: "food-rescue-social-platform.firebaseapp.com",
-    projectId: "food-rescue-social-platform",
-    storageBucket: "food-rescue-social-platform.appspot.com",
-    messagingSenderId: "684711937854",
-    appId: "1:684711937854:web:aa16c5dfb5ff0fe8f68e6f",
-    measurementId: "G-P1SGVSNEJL"
+    apiKey: API_KEY,
+    authDomain: AUTH_DOMAIN,
+    projectId: PROJECT_ID,
+    storageBucket: STORAGE_BUCKET,
+    messagingSenderId: MESSAGING_SENDER_ID,
+    appId: APP_ID,
+    measurementId: MEASUREMENT_ID
 };
 
 export const app = initializeApp(firebaseConfig);

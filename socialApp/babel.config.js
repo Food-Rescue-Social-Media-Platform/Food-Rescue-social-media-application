e18cebd4f676defd4fc459982ce55dd3e33678b1
mohammadBelbesi <my_1999@live.com>
2024-08-17 03:43:49 +0300

    try to solve the auto complete problem

--- socialApp/babel.config.js
+++ socialApp/babel.config.js
@@ -4,7 +4,17 @@ module.exports = function(api) {
     presets: ['babel-preset-expo'],
     plugins: [
       'react-native-reanimated/plugin',
-      ['module:react-native-dotenv']
+      [
+        'module:react-native-dotenv',
+        {
+          moduleName: '@env',  // This specifies the module name you will use to import the variables
+          path: '.env',        // This specifies the path to your .env file
+          blocklist: null,
+          allowlist: null,
+          safe: false,
+          allowUndefined: true,
+        },
+      ],
     ],
   };
 };

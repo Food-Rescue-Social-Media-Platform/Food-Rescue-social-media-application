2b3d3ce7608cd14b644f820a218a26825c950ac0
mohammadBelbesi <my_1999@live.com>
2024-08-17 07:57:34 +0300

    fix: app.config Cannot automatically write to dynamic config

--- socialApp/app.config.js
+++ socialApp/app.config.js
@@ -8,6 +8,7 @@ export default ({ config }) => ({
     "expo-router"
   ],
   android: {
+    package: "com.mohammad_belbesi.socialApp",  // Added the package name here
     config: {
       googleMaps: {
         apiKey: process.env.GOOGLE_MAPS_API_KEY
